136288884b0c56183eaa0d03b20b302b6e45d9cb
suzh <szh362680581@gmail.com>
2022-02-14 10:21:46 +0800

    update: `Product`组件增加单元测试

---
+++ src/components/Product/__test__/Product.test.js
@@ -0,0 +1,21 @@
+import { render } from '@testing-library/react';
+import Product from '..';
+
+const mockProduct = {
+  id: 12,
+  sku: 12064273040195392,
+  title: 'Cat Tee Black T-Shirt',
+  description: '4 MSL',
+  availableSizes: ['S', 'XS'],
+  style: 'Black with custom print',
+  price: 10.9,
+  installments: 9,
+  currencyId: 'USD',
+  currencyFormat: '$',
+  isFreeShipping: true,
+};
+
+test('`Product 正常渲染`', () => {
+  const { unmount } = render(<Product data={mockProduct} />);
+  unmount();
+});
